45ef7125989c897f1e8130243ea1020a0aff1beb
ansharw <wanshar9@gmail.com>
2021-09-29 15:58:52 +0700

    realtime v1-0-19-9-2021

--- application/controllers/Suhukelembaban.php
+++ application/controllers/Suhukelembaban.php
@@ -44,6 +44,7 @@ class Suhukelembaban extends Admin_Controller
     public function ambilData()
     {
         $data = $this->model_control->getAllData();
+        array_reverse($data);
         header('Content-Type: application/json');
         echo json_encode($data);  
     }
