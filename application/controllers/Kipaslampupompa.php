ba3875b16d3dfeffca4f2cfde43a6d74ad60914a
ansharw <wanshar9@gmail.com>
2021-10-11 07:12:22 +0700

    hacktoberfest-accepted

---
+++ application/controllers/Kipaslampupompa.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Kipaslampupompa extends Admin_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->not_logged_in();
+        $this->data['title'] = 'Fan, Lamp and Pump';
+        $this->load->model('model_control');
+    }
+
+    public function index($kipas_id = null, $lampu_id = null, $pompa_id = null)
+    {
+        $this->data['kipas'] = $this->model_control->getKipas($kipas_id);
+        $this->data['lampu'] = $this->model_control->getLampu($lampu_id);
+        $this->data['pompa'] = $this->model_control->getPompa($pompa_id);
+        $this->render_template('kipaslampupompa', $this->data);
+    }
+
+    public function ambilData()
+    {
+        $data = $this->model_control->getAllData();
+        header('Content-Type: application/json');
+        echo json_encode($data); 
+    }
+}
