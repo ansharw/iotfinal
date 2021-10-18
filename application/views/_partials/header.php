adbff752c47db94d7c753dc73773b064a5f53cf4
ansharw <wanshar9@gmail.com>
2021-10-18 19:11:18 +0700

    hacktoberfest-accepted

--- application/views/_partials/header.php
+++ application/views/_partials/header.php
@@ -13,37 +13,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
   <!-- General CSS Files -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/fontawesome/css/all.min.css">
-  <!-- <script src="<?php echo base_url(); ?>assets/modules/canvasjs.min.js"></script> -->
-  <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js@3.3.2"></script>
-  <script src="<?php echo base_url(); ?>assets/modules/jquery-3.3.1.min.js"></script> -->
-  <script src="https://cdn.jsdelivr.net/npm/luxon@1.27.0"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon@1.0.0"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
-  <!-- <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-streaming@2.0.0"></script> -->
-
-
   <!-- CSS Libraries -->
   <?php
   if ($this->uri->segment(1) == "" || $this->uri->segment(1) == "dashboard") { ?>
-    <!-- <script src="<?php echo base_url(); ?>assets/modules/chart.min.js"></script> -->
-    <!-- <script src="<?php echo base_url(); ?>assets/modules/canvasjs/canvasjs.min.js"></script> -->
-    <!-- <script src="<?php echo base_url(); ?>assets/modules/canvasjs/jquery.canvasjs.min.js"></script> -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/chocolat/dist/css/chocolat.css">
-    <!-- Datatable -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/datatables.min.css"> -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css"> -->
-    <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css"> -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/chocolat/dist/css/chocolat.css">
   <?php
   } elseif ($this->uri->segment(1) == "suhukelembaban") { ?>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/jquery-selectric/selectric.css">
-    <!-- <script src="<?php echo base_url(); ?>assets/modules/canvasjs/canvasjs.min.js"></script> -->
-    <!-- chartjs -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js@3.3.2"></script>
-      <script src="https://cdn.jsdelivr.net/npm/luxon@1.27.0"></script>
-      <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon@1.0.0"></script>
-      <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-streaming@2.0.0"></script> -->
-    <!-- datatable -->
+    <script src="https://cdn.jsdelivr.net/npm/luxon@1.27.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon@1.0.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/datatables.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
     <script src="<?php echo base_url(); ?>assets/modules/datatables/datatables.min.js"></script>
