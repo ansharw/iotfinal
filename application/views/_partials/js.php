227e315fe9426f7d06f4e5ce25d04afab9f16157
ansharw <wanshar9@gmail.com>
2021-10-19 20:40:12 +0700

    hacktoberfest-accepted

--- application/views/_partials/js.php
+++ application/views/_partials/js.php
@@ -147,7 +147,11 @@ if ($this->uri->segment(1) == "dashboard") { ?>
           $('#suhu1').html(htmlContent1);
         });
       }
-      recentS();
+
+      recentS = recentS();
+      setInterval(function() {
+        recentS.ajax.reload();
+      }, 30000);
     });
   </script>
 
